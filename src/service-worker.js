071a06f27a97d3605b6a2a60a7ebedfc8cbef233
alexdriaguine <alexdriagin12@gmail.com>
2018-12-03 21:35:30 +0100

    remove useless log

--- src/service-worker.js
+++ src/service-worker.js
@@ -3,6 +3,7 @@
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
+
 // These entries live outside of webpack. We build the index.html with the static
 // webpack compiler, and hence, it's not appended to the manifest entries from
 // our js-build
@@ -13,7 +14,6 @@ const manualEntries = [
 ]
 self.__precacheManifest = manualEntries.concat(self.__precacheManifest || [])
 
-console.log(self.__precacheManifest);
 workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
